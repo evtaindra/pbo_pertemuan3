8c4fc2e8b45406cba165eb3e7aaca42a1f3d6ba0
evtaindra <evtapribadi@gmail.com>
2025-10-22 18:38:30 +0700

    buat class Character

---
+++ Character.java
@@ -0,0 +1,30 @@
+//Buat nama Class sesuai dengan nama file
+public class Character {
+
+    //atribut = variabel = property
+    private String name;
+    private int level;
+    private int healthPoints;
+
+    //method = function
+    public void showLevel(){
+        System.out.println("Level: " +this.level);
+    }
+    public void upLevel(){
+        this.level +=1;
+    }
+    public void showName(){    
+        System.out.println("Name: " +this.name);  
+    }
+
+    public void showHealthPoints(){
+        System.out.println("Health Points: " +this.healthPoints);
+    }
+
+    //constructor
+    public Character(String xname,int xlevel,int xhealthPoints){
+        this.name = xname;
+        this.level = xlevel;
+        this.healthPoints = xhealthPoints;
+    }
+}
