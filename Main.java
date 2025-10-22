60f2c16e0de2c5b4c4ed8800f7df8a680246d5b7
evtaindra <evtapribadi@gmail.com>
2025-10-23 06:38:38 +0700

    implementasi Objek dengan Class Main

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        Character Cecilia = new Character("Cecilia",2,100);
+
+        Cecilia.showLevel();
+        Cecilia.showHealthPoints();
+        Cecilia.showName();
+        Cecilia.upLevel();
+    }
+}
